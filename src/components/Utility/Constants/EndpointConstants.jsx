2a1e059811725f79d619afe9fe949e3fded00cb4
alihanozdogan <65493677+alihanozdogan@users.noreply.github.com>
2023-01-02 11:55:51 +0300

    Update EndpointConstants.jsx

--- src/components/Utility/Constants/EndpointConstants.jsx
+++ src/components/Utility/Constants/EndpointConstants.jsx
@@ -1,3 +1,3 @@
 export const source = "http://ec2-3-75-224-255.eu-central-1.compute.amazonaws.com:8080";
-export const OAUTH2_REDIRECT_URI = 'https://footballerfe.netlify.app/oauth2/redirect';
+export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
 export const ACCESS_TOKEN = 'accessToken';
